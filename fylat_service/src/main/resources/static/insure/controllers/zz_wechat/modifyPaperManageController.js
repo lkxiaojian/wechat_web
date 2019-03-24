297136f5b8cbbeed4fc5d400c6a00e9ffe8d3be5
aspiresnow <714131254@qq.com>
2019-03-24 23:55:11 +0800

    修改编辑功能

--- fylat_service/src/main/resources/static/insure/controllers/zz_wechat/modifyPaperManageController.js
+++ fylat_service/src/main/resources/static/insure/controllers/zz_wechat/modifyPaperManageController.js
@@ -132,12 +132,14 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                         $scope.listObj.integrationQuery.content_excerpt_e = data.result.content_excerpt_e;
                         $scope.listObj.integrationQuery.article_keyword_e = data.result.article_keyword_e;
                         $scope.listObj.integrationQuery.article_title_e = data.result.article_title_e;
+                        $scope.listObj.integrationQuery.author_e = data.result.author_e;
                         $scope.listObj.integrationQuery.publication_date = data.result.publication_date;
                         $scope.listObj.integrationQuery.article_score = data.result.article_score;
                         $scope.listObj.integrationQuery.reference = data.result.reference;
 
                         $scope.listObj.integrationQuery.create_time = insureUtil.dateToString(new Date(data.result.create_time), "yyyy-MM-dd");
                         $scope.listObj.integrationQuery.update_time = insureUtil.dateToString(new Date(data.result.update_time), "yyyy-MM-dd");
+                        $scope.listObj.integrationQuery.check_type = data.result.check_type;
 
                         $scope.listObj.selectedItem.article_type_id = data.result.article_type_id;
                         $scope.listObj.selectedItem.article_type_name = data.result.article_type_name;
@@ -188,7 +190,7 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                         layer.closeAll('loading');
                         if (data.code == 0) {
                             layer.alert(data.message);
-                            $state.go($scope.pre_location);
+                            $state.go($scope.listObj.pre_location);
                         } else {
                             layer.alert(data.message);
                         }
@@ -217,7 +219,7 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                         layer.closeAll('loading');
                         if (data.code == 0) {
                             layer.alert(data.message);
-                            $state.go($scope.pre_location);
+                            $state.go($scope.listObj.pre_location);
                         } else {
                             layer.alert(data.message);
                         }
@@ -246,7 +248,7 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                         layer.closeAll('loading');
                         if (data.code == 0) {
                             layer.alert("发布成功");
-                            $state.go($scope.pre_location);
+                            $state.go($scope.listObj.pre_location);
                         } else {
                             layer.alert(data.message);
                         }
@@ -261,45 +263,56 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
 
         $scope.save = function(callback){
             if(!$scope.listObj.integrationQuery.article_title){
-                layer.tips("请输入标题","#article_title");
+                layer.msg("请输入标题");
+                // layer.tips("请输入标题","#article_title");
                 return;
             }
             if(!$scope.listObj.regionType.selected){
-                layer.tips("请选择类型","#article_type");
+                layer.msg("请选择类型");
+                // layer.tips("请选择类型","#article_type");
                 return;
             }
             if(!$scope.listObj.integrationQuery.source){
-                layer.tips("请输入来源","#source");
+                layer.msg("请输入来源");
+                // layer.tips("请输入来源","#source");
                 return;
             }
             if(!$scope.listObj.integrationQuery.create_time){
-                layer.tips("请选择发表时间","#create_time");
+                layer.msg("请选择发表时间");
+                // layer.tips("请选择发表时间","#create_time");
                 return;
             }
             if(!$scope.listObj.integrationQuery.update_time){
-                layer.tips("请选择入库时间","#update_time");
+                layer.msg("请选择入库时间");
+                // layer.tips("请选择入库时间","#update_time");
                 return;
             }
             if(!$scope.listObj.integrationQuery.article_keyword){
-                layer.tips("请输入关键字","#article_keyword");
+                layer.msg("请输入关键字");
+                // layer.tips("请输入关键字","#article_keyword");
                 return;
             }
             if(!$scope.listObj.integrationQuery.content_excerpt){
-                layer.tips("请输入摘要","#content_excerpt");
+                layer.msg("请输入摘要");
+                // layer.tips("请输入摘要","#content_excerpt");
                 return;
             }
             // 获取编辑器区域完整html代码
             var html = editor.txt.html();
             // 获取编辑器纯文本内容
             var text = editor.txt.text();
-            if (text == null || text.length == 0) {
-                layer.tips("请输入正文","#content");
-                return;
+            //只有文章的时候需要论文
+            if($scope.listObj.type == '0'){
+                if (text == null || text.length == 0) {
+                    layer.msg("请输入正文");
+                    return;
+                }
             }
 
             $scope.listObj.dataTime = angular.element('#time').val();
             var url = "releaseManagement/updateAricleTmpMesage/rest";
             if($scope.listObj.tmp_type == '1'){
+            // if($scope.listObj.type == '0' && $scope.listObj.tmp_type == '1'){
                 url = 'article/updateArticle';
             }
             layer.load(2);
@@ -331,6 +344,7 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                     content_excerpt_e: $scope.listObj.integrationQuery.content_excerpt_e,
                     article_keyword_e: $scope.listObj.integrationQuery.article_keyword_e,
                     article_title_e: $scope.listObj.integrationQuery.article_title_e,
+                    author_e: $scope.listObj.integrationQuery.author_e,
                     publication_date: $scope.listObj.integrationQuery.publication_date,
                     article_score: $scope.listObj.integrationQuery.article_score
 
@@ -343,7 +357,7 @@ app.controller('modifyPaperManageController', ['$scope', '$modal', '$http', 'fyl
                     if(callback){
                         callback();
                     }else{
-                        $state.go($scope.pre_location);
+                        $state.go($scope.listObj.pre_location);
                     }
                 } else {
                     layer.alert("更新失败");
