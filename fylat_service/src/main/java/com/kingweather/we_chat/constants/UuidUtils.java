5b6a3de97faf926a1a5a2aa0cb3525df3ca1e85b
lkxiaojian <18513075404@163.com>
2019-04-19 19:25:28 +0800

    文章和论文的抽取

--- fylat_service/src/main/java/com/kingweather/we_chat/constants/UuidUtils.java
+++ fylat_service/src/main/java/com/kingweather/we_chat/constants/UuidUtils.java
@@ -1,10 +1,33 @@
 package com.kingweather.we_chat.constants;
 
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 public class UuidUtils {
 
     public static String getUUid() {
         return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+
+    public static String guessEncoding(byte[] bytes) {
+        UniversalDetector detector = new UniversalDetector(null);
+        detector.handleData(bytes, 0, bytes.length);
+        detector.dataEnd();
+        String encoding = detector.getDetectedCharset();
+        detector.reset();
+        if (null != encoding) {
+            try {
+                return new String(bytes, encoding);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return new String(bytes);
+        }
+
+        return "";
     }
 }
